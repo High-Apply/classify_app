aeabacbec2dc790f5ee56510de720b55ff3ca383
Jun Seong Pyo <standardstar@hanyang.ac.kr>
2023-11-06 14:22:13 +0900

    추가 : 기본적 TabBar 및 TabView
    아직 탭바의 크기가 원하는 대로 조절되지 않고, indicator의 색깔도 다름.

--- lib/screens/timetable_screen.dart
+++ lib/screens/timetable_screen.dart
@@ -1,4 +1,4 @@
-import 'package:classify_app/widgets/my_app_bar.dart';
+import 'package:classify_app/styles/custom_font_style.dart';
 import 'package:flutter/material.dart';
 
 class TimetableScreen extends StatefulWidget {
@@ -11,12 +11,53 @@ class TimetableScreen extends StatefulWidget {
 class _TimetableScreenState extends State<TimetableScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: MyAppBar(
-        title: '주간 시간표',
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leadingWidth: 16,
+          leading: Container(),
+          title: const Text(
+            '주간 시간표',
+            style: CustomTextStyle.displayLarge,
+          ),
+          bottom: const TabBar(tabs: [
+            Tab(
+              child: Text(
+                "Week",
+                style: CustomTextStyle.titleSmall,
+              ),
+            ),
+            Tab(
+              child: Text(
+                "Day",
+                style: CustomTextStyle.titleSmall,
+              ),
+            ),
+            Tab(
+              child: Text(
+                "Lunch",
+                style: CustomTextStyle.titleSmall,
+              ),
+            ),
+          ]),
+        ),
+        body: const TabBarView(children: [
+          Center(
+            child: Text("IT IS ANDROID"),
+          ),
+          Center(
+            child: Text("IT IS MAC"),
+          ),
+          Center(
+            child: Text("IT IS RESTAURANT"),
+          ),
+        ]),
+        backgroundColor: Colors.white,
       ),
-      body: Center(child: Text("timetable Screen")),
-      backgroundColor: Colors.white,
     );
   }
 }
