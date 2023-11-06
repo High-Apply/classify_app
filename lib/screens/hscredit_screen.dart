92d507a4789b7c85ce1b4b70dce0709008630a6d
Jun Seong Pyo <standardstar@hanyang.ac.kr>
2023-11-06 14:05:55 +0900

    추가 : 캘린더/hscredit/설정 스크린 모두 앱바 추가 및 배경 색깔 하얀색으로 변경

--- lib/screens/hscredit_screen.dart
+++ lib/screens/hscredit_screen.dart
@@ -1,3 +1,4 @@
+import 'package:classify_app/widgets/my_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class HsCreditScreen extends StatelessWidget {
@@ -6,7 +7,11 @@ class HsCreditScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      appBar: MyAppBar(
+        title: '고교학점제',
+      ),
       body: Center(child: Text("hscredit Screen")),
+      backgroundColor: Colors.white,
     );
   }
 }
--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,3 +1,4 @@
+import 'package:classify_app/widgets/my_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class SettingScreen extends StatelessWidget {
@@ -6,7 +7,11 @@ class SettingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      appBar: MyAppBar(
+        title: '설정',
+      ),
       body: Center(child: Text("setting Screen")),
+      backgroundColor: Colors.white,
     );
   }
 }
